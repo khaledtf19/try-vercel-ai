082768484af6c8f0b17d1cb5144acea7467a3979
Khaledtf Labeb <khaledtf19@gmail.com>
2023-06-18 18:51:22 +0300

    update api POST

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,5 @@
 import { HfInference } from "@huggingface/inference";
+import { HuggingFaceStream, StreamingTextResponse } from "ai";
 
 const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 
@@ -21,4 +22,24 @@ function buildPompt(
       })
       .join("") + "<|assistant|>"
   );
+}
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  const response = Hf.textGenerationStream({
+    model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
+    inputs: buildPompt(messages),
+    parameters: {
+      max_new_tokens: 200,
+      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+      typical_p: 0.2,
+      repetition_penalty: 1,
+      truncate: 1000,
+      return_full_text: false,
+    },
+  });
+
+  const stream = HuggingFaceStream(response);
+
+  return new StreamingTextResponse(stream);
 }
